2bd1fb03cf2ee3676b79db06822fd794924a4433
Duke Leto <duke@leto.net>
2019-10-18 01:04:02 -0400

    make debian stuff happy

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -30,7 +30,7 @@ if [ -d $BUILD_DIR ]; then
     rm -R $BUILD_DIR
 fi
 
-DEB_BIN=$BUILD_DIR/usr/local/bin
+DEB_BIN=$BUILD_DIR/usr/bin
 DEB_CMP=$BUILD_DIR/usr/share/bash-completion/completions
 DEB_DOC=$BUILD_DIR/usr/share/doc/$PACKAGE_NAME
 DEB_MAN=$BUILD_DIR/usr/share/man/man1
@@ -44,15 +44,17 @@ chmod 0755 -R $BUILD_DIR/*
 #cp $SRC_DEB/prerm $BUILD_DIR/DEBIAN
 # Copy binaries. We prefix our komodod binaries with hush- to prevent conflicting with
 # a stock komodod or other flavors of KMD
-cp $SRC_PATH/src/komodod $DEB_BIN/hush-komodo
+cp $SRC_PATH/src/komodod $DEB_BIN/hush-komodod
 strip $DEB_BIN/hush-komodod
+cp $SRC_PATH/src/komodo-cli $DEB_BIN/hush-komodo-cli
+strip $DEB_BIN/hush-komodo-cli
+cp $SRC_PATH/src/komodo-tx $DEB_BIN/hush-komodo-tx
+strip $DEB_BIN/hush-komodo-tx
 cp $SRC_PATH/src/hushd $DEB_BIN
 cp $SRC_PATH/src/hush-cli $DEB_BIN
 cp $SRC_PATH/src/hush-tx $DEB_BIN
-cp $SRC_PATH/src/komodo-cli $DEB_BIN/hush-komodo-cli
-strip $DEB_BIN/hush-komodo-cli
 #cp $SRC_PATH/zcutil/fetch-params.sh $DEB_BIN/zcash-fetch-params
-cp $SRC_DEB/changelog $DEB_DOC/changelog.Debian
+#cp $SRC_DEB/changelog $DEB_DOC/changelog.Debian
 cp $SRC_DEB/copyright $DEB_DOC
 cp -r $SRC_DEB/examples $DEB_DOC
 # Copy manpages
@@ -71,14 +73,14 @@ cp $SRC_PATH/contrib/hushd.bash-completion $DEB_CMP/hushd
 cp $SRC_PATH/contrib/hush-cli.bash-completion $DEB_CMP/hush-cli
 cp $SRC_PATH/contrib/hush-tx.bash-completion $DEB_CMP/hush-tx
 # Gzip files
-gzip --best -n $DEB_DOC/changelog
-gzip --best -n $DEB_DOC/changelog.Debian
+#gzip --best -n $DEB_DOC/changelog
+#gzip --best -n $DEB_DOC/changelog.Debian
 gzip --best -n $DEB_MAN/hush-komodod.1
 gzip --best -n $DEB_MAN/hush-komodo-cli.1
 gzip --best -n $DEB_MAN/hush-komodo-tx.1
-gzip --best -n $DEB_MAN/hush-komodod.1
-gzip --best -n $DEB_MAN/hush-komodo-cli.1
-gzip --best -n $DEB_MAN/hush-komodo-tx.1
+gzip --best -n $DEB_MAN/hushd.1
+gzip --best -n $DEB_MAN/hush-cli.1
+gzip --best -n $DEB_MAN/hush-tx.1
 #gzip --best -n $DEB_MAN/zcash-fetch-params.1
 
 cd $SRC_PATH/contrib

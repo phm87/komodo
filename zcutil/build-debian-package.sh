8aaa92366fa8da7f69cc8dc29f436a5d602a196f
Jonathan "Duke" Leto <jonathan@leto.net>
2019-10-17 21:08:46 -0700

    debianizing

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -3,10 +3,12 @@
 ## Usage:
 ##  ./zcutil/build-debian-package.sh
 
+echo "Let There Be Debian Packages"
+
 set -e
 set -x
 
-BUILD_PATH="/tmp/hush-deb"
+BUILD_PATH="/tmp/hush-debian-$$"
 PACKAGE_NAME="hush"
 SRC_PATH=`pwd`
 SRC_DEB=$SRC_PATH/contrib/debian
@@ -28,7 +30,7 @@ if [ -d $BUILD_DIR ]; then
     rm -R $BUILD_DIR
 fi
 
-DEB_BIN=$BUILD_DIR/usr/bin
+DEB_BIN=$BUILD_DIR/usr/local/bin
 DEB_CMP=$BUILD_DIR/usr/share/bash-completion/completions
 DEB_DOC=$BUILD_DIR/usr/share/doc/$PACKAGE_NAME
 DEB_MAN=$BUILD_DIR/usr/share/man/man1
@@ -40,37 +42,49 @@ chmod 0755 -R $BUILD_DIR/*
 #cp $SRC_DEB/postrm $BUILD_DIR/DEBIAN
 #cp $SRC_DEB/preinst $BUILD_DIR/DEBIAN
 #cp $SRC_DEB/prerm $BUILD_DIR/DEBIAN
-# Copy binaries
-cp $SRC_PATH/src/komodod $DEB_BIN
-strip $DEB_BIN/komodod
-cp $SRC_PATH/src/verusd $DEB_BIN
-cp $SRC_PATH/src/komodo-cli $DEB_BIN
-strip $DEB_BIN/komodo-cli
-cp $SRC_PATH/src/verus $DEB_BIN
-cp $SRC_PATH/zcutil/fetch-params.sh $DEB_BIN/zcash-fetch-params
-# Copy docs
-cp $SRC_PATH/doc/release-notes/release-notes-1.0.0.md $DEB_DOC/changelog
+# Copy binaries. We prefix our komodod binaries with hush- to prevent conflicting with
+# a stock komodod or other flavors of KMD
+cp $SRC_PATH/src/komodod $DEB_BIN/hush-komodo
+strip $DEB_BIN/hush-komodod
+cp $SRC_PATH/src/hushd $DEB_BIN
+cp $SRC_PATH/src/hush-cli $DEB_BIN
+cp $SRC_PATH/src/hush-tx $DEB_BIN
+cp $SRC_PATH/src/komodo-cli $DEB_BIN/hush-komodo-cli
+strip $DEB_BIN/hush-komodo-cli
+#cp $SRC_PATH/zcutil/fetch-params.sh $DEB_BIN/zcash-fetch-params
 cp $SRC_DEB/changelog $DEB_DOC/changelog.Debian
 cp $SRC_DEB/copyright $DEB_DOC
 cp -r $SRC_DEB/examples $DEB_DOC
 # Copy manpages
-cp $SRC_DOC/man/komodod.1 $DEB_MAN
-cp $SRC_DOC/man/komodo-cli.1 $DEB_MAN
-cp $SRC_DOC/man/zcash-fetch-params.1 $DEB_MAN
+cp $SRC_DOC/man/komodod.1 $DEB_MAN/hush-komodod.1
+cp $SRC_DOC/man/komodo-cli.1 $DEB_MAN/hush-komodo-cli.1
+cp $SRC_DOC/man/komodo-tx.1 $DEB_MAN/hush-komodo-tx.1
+
+cp $SRC_DOC/man/komodod.1 $DEB_MAN/hushd.1
+cp $SRC_DOC/man/komodo-cli.1 $DEB_MAN/hush-cli.1
+cp $SRC_DOC/man/komodo-tx.1 $DEB_MAN/hush-tx.1
+#TODO: process these copies and update names/binaries/etc
+
+#cp $SRC_DOC/man/zcash-fetch-params.1 $DEB_MAN
 # Copy bash completion files
-cp $SRC_PATH/contrib/zcashd.bash-completion $DEB_CMP/zcashd
-cp $SRC_PATH/contrib/zcash-cli.bash-completion $DEB_CMP/zcash-cli
+cp $SRC_PATH/contrib/hushd.bash-completion $DEB_CMP/hushd
+cp $SRC_PATH/contrib/hush-cli.bash-completion $DEB_CMP/hush-cli
+cp $SRC_PATH/contrib/hush-tx.bash-completion $DEB_CMP/hush-tx
 # Gzip files
 gzip --best -n $DEB_DOC/changelog
 gzip --best -n $DEB_DOC/changelog.Debian
-gzip --best -n $DEB_MAN/komodod.1
-gzip --best -n $DEB_MAN/komodo-cli.1
-gzip --best -n $DEB_MAN/zcash-fetch-params.1
+gzip --best -n $DEB_MAN/hush-komodod.1
+gzip --best -n $DEB_MAN/hush-komodo-cli.1
+gzip --best -n $DEB_MAN/hush-komodo-tx.1
+gzip --best -n $DEB_MAN/hush-komodod.1
+gzip --best -n $DEB_MAN/hush-komodo-cli.1
+gzip --best -n $DEB_MAN/hush-komodo-tx.1
+#gzip --best -n $DEB_MAN/zcash-fetch-params.1
 
 cd $SRC_PATH/contrib
 
 # Create the control file
-dpkg-shlibdeps $DEB_BIN/komodod $DEB_BIN/komodo-cli
+dpkg-shlibdeps $DEB_BIN/hush-komodod $DEB_BIN/hush-komodo-cli
 dpkg-gencontrol -P$BUILD_DIR -v$DEBVERSION
 
 # Create the Debian package

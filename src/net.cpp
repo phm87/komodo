719a8d6f20d0682052fdb8b78236202dda64a0a1
Jonathan "Duke" Leto <jonathan@leto.net>
2019-10-23 06:54:45 -0700

    More logging for dns seed stuff

--- src/net.cpp
+++ src/net.cpp
@@ -1319,6 +1319,9 @@ void ThreadDNSAddressSeed()
                     {
                         vAdd.push_back(addr);
                         found++;
+                        LogPrintf("%s: Added node via DNS seed: %s:%d", __FUNCTION__, ip.c_str(), ASSETCHAINS_P2PPORT);
+                    } else {
+                        LogPrintf("%s: Node has incorrect port %s:%d", __FUNCTION__, ip.c_str(), addr.GetPort() );
                     }
                 }
             }

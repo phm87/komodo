b6fa28dfa27e536375f7a5f7b6c5a721add519b3
Duke Leto <duke@leto.net>
2019-12-11 15:51:40 -0500

    Delete more dead code that causes compile issues

--- src/pow.h
+++ src/pow.h
@@ -37,8 +37,6 @@ unsigned int CalculateNextWorkRequired(arith_uint256 bnAvg,
                                        int64_t nLastBlockTime, int64_t nFirstBlockTime,
                                        const Consensus::Params&);
 
-unsigned int lwmaGetNextPOSRequired(const CBlockIndex* pindexLast, const Consensus::Params& params);
-
 /** Check whether the Equihash solution in a block header is valid */
 bool CheckEquihashSolution(const CBlockHeader *pblock, const CChainParams&);
 

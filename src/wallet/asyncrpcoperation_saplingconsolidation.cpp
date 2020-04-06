a7dd309ad075f51bacdc8dcdd627cbdb42847698
Duke Leto <duke@leto.net>
2020-04-06 18:37:02 -0400

    Add copyright

--- src/wallet/asyncrpcoperation_saplingconsolidation.cpp
+++ src/wallet/asyncrpcoperation_saplingconsolidation.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2020 The Hush developers
-// TODO: Forge should add his preferred copyright line here
+// Copyright (c) 2019 CryptoForge
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 

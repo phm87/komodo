0c118eb129f06ec6a9ff5ca9750b75b3417a8a2d
Duke Leto <duke@leto.net>
2020-06-07 00:04:15 -0400

    delete

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -23,7 +23,7 @@
 #define BITCOIN_PRIMITIVES_BLOCK_H
 
 #include "primitives/transaction.h"
-#include "primitives/nonce.h"
+//#include "primitives/nonce.h"
 #include "serialize.h"
 #include "uint256.h"
 #include "arith_uint256.h"

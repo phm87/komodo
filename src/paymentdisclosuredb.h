f4dd9fe3bbd6b57cd55d5b638322028e54dba3e9
Duke Leto <duke@leto.net>
2020-06-13 08:50:52 -0400

    desprout

---
+++ src/paymentdisclosuredb.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2017 The Zcash developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ZCASH_PAYMENTDISCLOSUREDB_H
+#define ZCASH_PAYMENTDISCLOSUREDB_H
+
+#include "paymentdisclosure.h"
+
+#include <cstdint>
+#include <string>
+#include <mutex>
+#include <future>
+#include <memory>
+
+#include <boost/optional.hpp>
+
+#include <leveldb/db.h>
+
+
+#endif // ZCASH_PAYMENTDISCLOSUREDB_H

86a925afe192d1045947bda9cf61484c667daeaa
phm87 <31578435+phm87@users.noreply.github.com>
2020-08-14 00:20:15 +0200

    Update coins.cpp

--- src/coins.cpp
+++ src/coins.cpp
@@ -613,7 +613,7 @@ CAmount CCoinsViewCache::GetValueIn(int32_t nHeight,int64_t *interestp,const CTr
         nResult += value;
 #ifdef KOMODO_ENABLE_INTEREST
         if ( (ASSETCHAINS_SYMBOL[0] == 0 && nHeight >= 60000 && ( value >= 10*COIN ))
-            || (ASSETCHAINS_ACTIVEUSERREWARD[0] == 1 && nHeight >= ASSETCHAINS_ACTIVEUSERREWARD[1] && ( value >= ASSETCHAINS_ACTIVEUSERREWARD[2]*COIN ))
+            || (ASSETCHAINS_ACTIVEUSERREWARD[0] == 1 && nHeight >= ASSETCHAINS_ACTIVEUSERREWARD[1] && ( value >= ASSETCHAINS_ACTIVEUSERREWARD[2]*COIN )))
         {
             {
                 int64_t interest; int32_t txheight; uint32_t locktime;

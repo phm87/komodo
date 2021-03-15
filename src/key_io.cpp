13173e6e9e989de541c3c5489fa33b6858e05553
phm87 <31578435+phm87@users.noreply.github.com>
2021-03-15 20:26:57 +0100

    Update key_io.cpp

--- src/key_io.cpp
+++ src/key_io.cpp
@@ -76,7 +76,7 @@ CTxDestination DecodeDestination(const std::string& str, const CChainParams& par
     return CNoDestination();
 }
 
-CTxDestination DecodeDestination3(const std::string& str, const int pubkey_prefix, const int script_prefix)
+CTxDestination DecodeDestination3(const std::string& str, const std::string& pubkey_prefix, const std::string& script_prefix)
 {
     std::vector<unsigned char> data;
     uint160 hash;
@@ -87,7 +87,7 @@ CTxDestination DecodeDestination3(const std::string& str, const int pubkey_prefi
         // std::vector<unsigned char> pubkey_prefix_v(pubkey_prefix.begin(), pubkey_prefix.end());
         // std::vector<unsigned char> script_prefix_v(script_prefix.begin(), script_prefix.end());
         // const std::vector<unsigned char>& pubkey_prefix = params.Base58Prefix(CChainParams::PUBKEY_ADDRESS);
-        const std::vector<unsigned char>& pubkey_prefix_v = std::vector<unsigned char>(1,pubkey_prefix);
+        const std::vector<unsigned char>& pubkey_prefix_v = std::vector<unsigned char>(1,atoi(pubkey_prefix));
         if (data.size() == hash.size() + pubkey_prefix.size() && std::equal(pubkey_prefix_v.begin(), pubkey_prefix_v.end(), data.begin())) {
             std::copy(data.begin() + pubkey_prefix_v.size(), data.end(), hash.begin());
             return CKeyID(hash);
@@ -95,7 +95,7 @@ CTxDestination DecodeDestination3(const std::string& str, const int pubkey_prefi
         // Script-hash-addresses have version 5 (or 196 testnet).
         // The data vector contains RIPEMD160(SHA256(cscript)), where cscript is the serialized redemption script.
         // const std::vector<unsigned char>& script_prefix = params.Base58Prefix(CChainParams::SCRIPT_ADDRESS);
-        const std::vector<unsigned char>& script_prefix_v = std::vector<unsigned char>(1,script_prefix);
+        const std::vector<unsigned char>& script_prefix_v = std::vector<unsigned char>(1,atoi(script_prefix));
         if (data.size() == hash.size() + script_prefix_v.size() && std::equal(script_prefix_v.begin(), script_prefix_v.end(), data.begin())) {
             std::copy(data.begin() + script_prefix_v.size(), data.end(), hash.begin());
             return CScriptID(hash);
@@ -335,7 +335,7 @@ CTxDestination DecodeDestination(const std::string& str)
 }
 
 
-CTxDestination DecodeDestination2(const std::string& str, const int pubkey_prefix, const int script_prefix)
+CTxDestination DecodeDestination2(const std::string& str, const std::string& pubkey_prefix, const std::string& script_prefix)
 {
     return DecodeDestination3(str, pubkey_prefix, script_prefix);
 }

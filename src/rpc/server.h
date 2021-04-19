501d0c74a88ccd9331246d597faa54ef8f4e3931
phm87 <31578435+phm87@users.noreply.github.com>
2021-04-20 01:50:47 +0200

    Update server.h

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -227,6 +227,7 @@ extern UniValue importprivkey(const UniValue& params, bool fHelp, const CPubKey&
 extern UniValue importaddress(const UniValue& params, bool fHelp, const CPubKey& mypk);
 extern UniValue dumpwallet(const UniValue& params, bool fHelp, const CPubKey& mypk);
 extern UniValue importwallet(const UniValue& params, bool fHelp, const CPubKey& mypk);
+extern UniValue pubkey2addr_rpc(const UniValue& params, bool fHelp);
 
 extern UniValue getgenerate(const UniValue& params, bool fHelp, const CPubKey& mypk); // in rpcmining.cpp
 extern UniValue setgenerate(const UniValue& params, bool fHelp, const CPubKey& mypk);
